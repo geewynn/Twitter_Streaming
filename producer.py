09343be5b806f8e458996cedb176c28095ce4d9b
Godwin <ekainugodwin@gmail.com>
2020-08-03 13:21:26 +0100

    stream with producer.py

--- producer.py
+++ producer.py
@@ -8,23 +8,19 @@ access_token_secret = "9PolB0nXPWA6Y6j5kYdpzqIKgIyE1lA1RIxKeKXZnIBcJ"
 consumer_key = "ow7stEAStsyNDqc5u51YphqDc"
 consumer_secret = "2HMj5hQ0M1GPvocsgCl3skFwUvRDn8A91Pi4FGi2fHGEhqMbVk"
 
-def twitterauth(consumer_key,consumer_secret,access_token,access_token_secret):
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    return auth
-
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-topic_name = "twitter"
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        producer.send("twitter", data.encode('utf-8'))
+        return True
+    def on_error(self, status):
+        print(status)
 
-def TwitterStreamer():
-        while True:
-            listener = ListenerTS() 
-            auth = twitterauth()
-            stream = Stream(auth, listener)
-            stream.filter(track=["#BBNaijaLockdown"], stall_warnings=True, languages= ["en"])
 
-def ListenerTS(raw_data):
-            producer.send(topic_name, str.encode(raw_data))
-            return True
+listener= StdOutListener()
+stream = Stream(auth, listener)
+stream.filter(track="BBNaijaLockdown")
